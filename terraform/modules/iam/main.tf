8a2a05d1c95a97ee7e2517d02aedc2b886119c75
Nebat-Taha <nebatn@gmail.com>
2026-03-28 16:19:28 -0400

    terraform module refactored- 1

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -49,11 +49,3 @@ resource "aws_iam_instance_profile" "this" {
   role = aws_iam_role.this.name
 }
 
-# ==============================================================================
-# OUTPUTS: Essential for passing the profile name to the EC2 module
-# ==============================================================================
-
-output "instance_profile_name" {
-  value       = aws_iam_instance_profile.this.name
-  description = "The name of the instance profile to be used in the EC2 module."
-}
--- terraform/modules/iam/outputs.tf
+++ terraform/modules/iam/outputs.tf
@@ -1,4 +1,8 @@
-# outputs.tf
+# ==============================================================================
+# OUTPUTS: Essential for passing the profile name to the EC2 module
+# ==============================================================================
+
 output "instance_profile_name" {
-  value = aws_iam_instance_profile.prometheus_profile.name
+  value       = aws_iam_instance_profile.this.name
+  description = "The name of the instance profile to be used in the EC2 module."
 }
